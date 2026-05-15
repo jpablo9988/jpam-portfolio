78c3462495f29e6d61b5745fb1672bdae44aab2f
Juan Pablo Amorocho <jpablo9988@gmail.com>
2026-05-15 16:30:21 -0400

    New base on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://jpablo9988.github.io/jpam-portfoliowebsite/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
